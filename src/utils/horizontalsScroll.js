c6b3110240e5684956fee3ae46e13c5bf808db00
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2022-11-29 17:38:57 +0200

    fix scroll issue

--- src/utils/horizontalsScroll.js
+++ src/utils/horizontalsScroll.js
@@ -1,6 +1,6 @@
 import { isIOS } from 'react-device-detect';
 
-export default function horizontalsScroll(array, tapIdPrefix, scrollWrapperId, buttonIndex, actualElementWidth) {
+export default function horizontalsScroll(array, tapIdPrefix, scrollWrapperId, buttonIndex, actualElementWidth, leftScrollValue2) {
   const currentPath = window.location.pathname;
   const activeTapId = buttonIndex === 0 ? 0 : buttonIndex || array?.filter((item) => item.path === currentPath)?.[0]?.id
   const scrollWrapper = window.document.querySelector(`#${scrollWrapperId}`);
@@ -8,12 +8,17 @@ export default function horizontalsScroll(array, tapIdPrefix, scrollWrapperId, b
   if (scrollWrapper) {
     if (activeTapId > 2) {
       setTimeout(() => {
-        const elementWidth = window.document.querySelector(`#${tapIdPrefix}${activeTapId}`)?.offsetWidth;
-        const leftScrollValue = ((activeTapId - 1.5) * (actualElementWidth || elementWidth))
+        const elementWidth = window.document.querySelector(`#${tapIdPrefix}${activeTapId}`)?.offsetLeft;
+        const leftScrollValue = leftScrollValue2 || (elementWidth - 621)
+          // console.log('leftScrollValue2 || leftScrollValue leftScrollValue1');
+          // console.log('leftScrollValue', leftScrollValue, leftScrollValue2, (elementWidth - 621));
+          
+            window.uss.scrollXTo(leftScrollValue, scrollWrapper)
+          
         if (isIOS) {
           window.uss.setMinAnimationFrame(20)
         }
-        window.uss.scrollXTo(leftScrollValue, scrollWrapper)
+        // console.log('leftScrollValue2 || leftScrollValue',leftScrollValue2 , '', leftScrollValue, '');
       }, 0)
     } else {
       scrollWrapper.scrollLeft = 0;
